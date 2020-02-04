d796341a8183c37f9aa9f3ab8f3f51f054cb23d3
jstephensasij <45579926+jstephensasij@users.noreply.github.com>
2020-02-04 09:23:23 +0900

    Update RGBLED.py

--- RGBLED.py
+++ RGBLED.py
@@ -2,28 +2,28 @@ from microbit import *
 from random import randint
 
 redVal = 0
-greenVal = 255
-blueVal = 255
+greenVal = 1023
+blueVal = 1023
 
 while True:
     if button_a.is_pressed():
-        redVal = 255
-        blueVal = 255
+        redVal = 1023
+        blueVal = 1023
         greenVal = 0
         pin0.write_analog(redVal)
         pin1.write_analog(greenVal)
         pin2.write_analog(blueVal)
     elif button_b.is_pressed():
-        redVal = 255
+        redVal = 1023
         blueVal = 0
-        greenVal = 255
+        greenVal = 1023
         pin0.write_analog(redVal)
         pin1.write_analog(greenVal)
         pin2.write_analog(blueVal)
     else:
+        redVal = 0
+        greenVal = 1023
+        blueVal = 1023
         pin0.write_analog(redVal)
         pin1.write_analog(greenVal)
         pin2.write_analog(blueVal)
-    redVal = 0
-    greenVal = 255
-    blueVal = 255
